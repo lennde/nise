8df5bb2983062c10af4947f7d0370fe120d8fdc7
Lennard DG <lenndg@gmail.com>
2019-01-25 03:46:58 +0100

    - correct timing sounds

--- __init__.py
+++ __init__.py
@@ -4,6 +4,8 @@ from flask import render_template
 from controller import Context
 from forms import StartForm
 
+import json
+
 server = Flask(__name__)
 server.secret_key = 'dAdinai99adn9N)Dna9d'
 server.config['UPLOAD_FOLDER'] = "static/uploads"
@@ -26,8 +28,13 @@ def index():
         # Create Participant and Sound Planning
         participant = g.env.create_participant(form.name.data)
         sound_planning = g.env.generate_sounds(participant.studyparticipant_id)
-        return render_template("experiment.html", participant=participant, sound_planning=sound_planning)
+        return render_template("experiment.html", participant=participant)
     return render_template("welcome.html", form=form)
+
+
+@server.route("/sound_planning/<participant_id>")
+def sound_planning(participant_id):
+    return json.dumps({ "sound_planning": g.env.get_sound_planning(participant_id)})
 
 
 if __name__ == "__main__":
--- controller.py
+++ controller.py
@@ -19,20 +19,28 @@ class Context:
 
     def generate_sounds(self, studyparticipant_id, chance=0.50):
         sound_planning = []
-        s = 1
-        while s < 60*60:
-            s += randint(1, 5)
+        sec = 1
+        while sec < 60*60:
+            sec += randint(1, 5)
             if random() <= chance:
                 sound = 1
-                s += 2
+                sec += 2
             else:
                 sound = 2
-                s += 2
+                sec += 2
             sound_planning.append(data_model.SoundPlanning(sound=sound, studyparticipant_id=studyparticipant_id,
-                                                           start_time=s))
+                                                           start_time=sec))
         self.session.bulk_save_objects(sound_planning)
         self.session.commit()
         return sound_planning
+
+    def get_sound_planning(self, participant_id):
+        planning = []
+        sound_plannings = self.session.query(data_model.SoundPlanning)\
+                                      .filter(data_model.SoundPlanning.studyparticipant_id == participant_id).all()
+        for sound_planning in sound_plannings:
+            planning.append({'start_time': sound_planning.start_time, 'sound': sound_planning.sound})
+        return planning
 
     def setup(self):
         data_model.create_db()
